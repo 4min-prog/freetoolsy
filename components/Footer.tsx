e8a51bff1f5375d4bec84a24280c5d9dcc19ad1c
Muhammed Emin <mail@example.com>
2026-09-24 20:10:10 +0300

    Footer'a LaunchNest rozeti eklendi: LinklyhHub partner notunun yanina, rel=noopener (nofollow/sponsored/ugc yok)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -77,6 +77,20 @@ export default async function Footer() {
                   {t("linkly")}
                 </a>
               </li>
+              <li className="pt-1">
+                <a
+                  href="https://launchnest.io/p/freetoolsy"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <img
+                    src="https://launchnest.io/brand/badges/listed-dark.svg"
+                    alt="Listed on LaunchNest"
+                    width="150"
+                    className="h-auto rounded-md border border-border bg-surface transition-opacity hover:opacity-80"
+                  />
+                </a>
+              </li>
             </ul>
           </div>
         </nav>
